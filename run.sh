52b2224f776aaf0c0217c51bf1a84237230be9cb
Your Name <you@example.com>
2024-02-11 21:00:18 -0800

    [Initial] commit from window
    [Updated] run.sh

--- run.sh
+++ run.sh
@@ -3,25 +3,7 @@
 # this script is for replacing Makefile
 # run this in gitbash if you are windows user
 
-
-
-all() {
-    if [[ ! -d "virtual" || $1 == "force" ]]; then
-        echo "Creating Virtual Environment..."
-        python -m venv virtual || {
-            echo "Error: Failed to create virtual environment."
-            return 1
-        }
-    fi
-
-    echo "Activating the Virtual Environment [virtual]..."
-    source ./virtual/Scripts/activate || {
-        echo "Error: Failed to activate virtual environment."
-        return 1
-    }
-
-    echo "Virtual Environment [virtual] is activated."
-
+install_dependency(){
     if [[ -f "requ.txt" ]]; then
         echo "Installing dependencies from [requ.txt]..."
         python -m pip install -r ./requ.txt || {
@@ -32,6 +14,37 @@ all() {
     else
         echo "Warning: No 'requ.txt' file found. Skipping dependency installation."
     fi
+
+}
+
+activate_venv(){
+    if [[ ! -d "virtual" || $1 == "force" ]]; then
+        echo "Creating Virtual Environment..."
+        python -m venv virtual || {
+            echo "Error: Failed to create virtual environment."
+            return 1
+        }
+        install_dependency || {
+            echo "Failed To install dependency Please use 'force' next time"
+        }
+    fi
+
+    if [[ -n "$VIRTUAL_ENV" ]]; then
+        echo "Activating the Virtual Environment [virtual]..."
+        source ./virtual/Scripts/activate || {
+            echo "Error: Failed to activate virtual environment."
+            return 1
+        }
+    fi
+
+    echo "Virtual Environment [virtual] is activated."
+
+}
+
+all() {
+    
+    activate_venv $@
+    
 
     if [[ -n "$VIRTUAL_ENV" ]]; then
         python ./src/main.py "$@"
